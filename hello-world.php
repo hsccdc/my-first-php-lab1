9d2bfe52390f07727d1733208579b1778877cb9a
kherring-dev1 <kherring@enlightened.com>
2021-05-01 13:58:18 -0400

    I completed my first PHP lab

---
+++ hello-world.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+  <title>PHP Lab 1</title>
+</head>
+<body>
+<?php 
+  //This is how we display Hello World to the user
+  echo "<p>Hello World</p>";
+  echo "<p>This is cool!</p>";
+  
+  //Let's make a variable!
+  $myName1 = "Kenya";
+  echo "<p>My name is $myName1 .</p>";
+  $myName2 = "Mya";
+  echo "<p>My name is $myName2 .</p>";
+  $myName3 = "Gnima";
+  echo "<p>My name is $myName3 .</p>";
+
+  echo "<p>I have three names. My first name is ", $myName1 , 
+  " My second name is ", $myName2 , " My third name is ", $myName3;
+?>
+</body>
+</html>
